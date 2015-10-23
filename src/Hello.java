c967dedb2739844a5b786f0faf6f8dac0b0b23c4
Alix <Alix_huang@sina.cn>
2015-10-23 16:32:49 +0800

    Test add

---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author waiting, 2015年10月23日 下午4:30:58
+ */
+
+/**
+ * 
+ * @author waiting, 2015年10月23日 下午4:30:58
+ */
+public class Hello {
+
+    /**
+     * 
+     * @author waiting, 2015年10月23日 下午4:30:58
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO 此方法尚未实现
+
+    }
+
+}
