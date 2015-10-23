e70372328cd88fc65af1b1d8ad7991d19d5d341c
Alix <Alix_huang@sina.cn>
2015-10-23 16:59:10 +0800

    delete useless

--- src/com/ming/ketuo/webserviceforfind/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.keytop.com.cn/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.ming.ketuo.webserviceforfind;
